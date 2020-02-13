acf929a63eca4348925b2004e03738e9649d47a4
Yancey Leo <yanceyofficial@gmail.com>
2020-02-14 02:12:56 +0800

    feat: use lib

---
+++ bin/cli.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+const commander = require('commander')
+const bootstrap = require('../lib/bootstrap')
--- scripts/bootstrap.js
+++
@@ -1,8 +0,0 @@
-const chalk = require('chalk')
-
-const inputTemplateChoices = require('./templateChoices')
-const inputFeatureChoices = require('./featureChoices')
-
-console.log('\n')
-console.log(chalk.bold('Vue CLI v4.2.2'))
-inputTemplateChoices()

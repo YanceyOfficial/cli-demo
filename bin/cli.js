e273c68a783cd2307b66e50529e23b5f2414aa58
Yancey Leo <yanceyofficial@gmail.com>
2020-02-14 02:28:41 +0800

    feat: update cli

--- bin/cli.js
+++ bin/cli.js
@@ -1,4 +1,51 @@
 #!/usr/bin/env node
 
-const commander = require('commander')
-const bootstrap = require('../lib/bootstrap')
+const program = require('commander')
+
+program.version('@vue/cli 4.3.2').usage('<command> [options]')
+
+program
+  .command('create <app-name>')
+  .description('create a new project powered by vue-cli-service')
+  .option('-p, --preset <presetName>', 'Skip prompts and use saved or remote preset')
+  .option('-d, --default', 'Skip prompts and use default preset')
+  .action((name, cmd) => {
+    require('../lib/bootstrap')()
+  })
+
+program
+  .command('serve [entry]')
+  .description('serve a .js or .vue file in development mode with zero config')
+  .option('-o, --open', 'Open browser')
+  .option('-c, --copy', 'Copy local url to clipboard')
+  .option('-p, --port <port>', 'Port used by the server (default: 8080 or next available port)')
+  .action((entry, cmd) => {
+    require('../lib/bootstrap')()
+  })
+
+program
+  .command('build [entry]')
+  .description('build a .js or .vue file in production mode with zero config')
+  .option('-t, --target <target>', 'Build target (app | lib | wc | wc-async, default: app)')
+  .option('-n, --name <name>', 'name for lib or web-component mode (default: entry filename)')
+  .option('-d, --dest <dir>', 'output directory (default: dist)')
+  .action((entry, cmd) => {
+    require('../lib/bootstrap')()
+  })
+
+// output help information on unknown commands
+program.arguments('<command>').action(cmd => {
+  program.outputHelp()
+  console.log(`  ` + chalk.red(`Unknown command ${chalk.yellow(cmd)}.`))
+  console.log()
+  suggestCommands(cmd)
+})
+
+// add some useful info on help
+program.on('--help', () => {
+  console.log()
+  console.log(`  Run ${chalk.cyan(`vue <command> --help`)} for detailed usage of given command.`)
+  console.log()
+})
+
+program.commands.forEach(c => c.on('--help', () => console.log()))
